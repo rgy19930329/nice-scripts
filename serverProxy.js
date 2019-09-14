31ab566cbf082cb22b8409b20e9263a4e04d022d
ranguangyu <593289488@qq.com>
2019-09-14 17:50:59 +0800

    add serverProxy

---
+++ serverProxy.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "/example/*": {
+    target: "https://easy-mock.com/mock/5ad07a89909da41e79f4a8a3",
+    changeOrigin: true,
+  },
+  "/sug": {
+    target: "https://suggest.taobao.com",
+    changeOrigin: true,
+  },
+  "/novelSearchApi": {
+    target: "https://www.apiopen.top",
+    changeOrigin: true,
+  },
+  "/yapi": {
+    target: "http://yapi.demo.qunar.com/mock/94215",
+    changeOrigin: true,
+  }
+}
